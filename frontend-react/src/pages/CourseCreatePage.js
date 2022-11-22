2e458c0a18aa10de52bf81bd30ff95878874582f
IsakLundstrom <isalun-9@student.ltu.se>
2022-11-22 10:44:22 +0100

    create course from react test

--- frontend-react/src/pages/CourseCreatePage.js
+++ frontend-react/src/pages/CourseCreatePage.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CourseCreatePage = () => {
+  const [cName, setCName] = useState({});
+  const [shortDescription, setShortDescription] = useState({});
+  const [longDescription, setLongDescription] = useState({});
+  const [cImage, setCImage] = useState({});
+
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+
+    let res = await fetch("http://localhost:8000/courses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        courseName: cName,
+        shortDescription: shortDescription,
+        longDescription: longDescription,
+      }),
+    });
+  };
+
   return (
     <main>
       <h1>Create Course</h1>
-      <form action="">
-        <label for="name">Name</label>
+      <form onSubmit={handleSubmit}>
+        <label for="cname">Name</label>
         <br />
-        <input className="inputField" required type="text" name="name" />
+        <input
+          className="inputField"
+          required
+          type="text"
+          name="cname"
+          value={cName}
+          onChange={(e) => setCName(e.target.value)}
+        />
 
         <br />
 
@@ -18,6 +44,8 @@ const CourseCreatePage = () => {
           required
           type="text"
           name="shortDescription"
+          value={shortDescription}
+          onChange={(e) => setShortDescription(e.target.value)}
         />
 
         <br />
@@ -29,7 +57,22 @@ const CourseCreatePage = () => {
           required
           name="longDescription"
           rows="5"
+          value={longDescription}
+          onChange={(e) => setLongDescription(e.target.value)}
         ></textarea>
+
+        <br />
+
+        <label for="image">Image</label>
+        <br />
+        <input
+          className=""
+          required
+          type="file"
+          name="image"
+          value={cImage}
+          onChange={(e) => setCImage(e.target.value)}
+        />
 
         <br />
         <br />
