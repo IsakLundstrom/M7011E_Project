d5d74e75c030b480d66a9c4abe54afcaad8cc277
IsakLundstrom <isalun-9@student.ltu.se>
2022-12-14 16:07:34 +0100

    Added google login, kanske fungerar :)

--- frontend-react/src/pages/LoginPage.js
+++ frontend-react/src/pages/LoginPage.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
+
 // import jwt_decode from "jwt-decode";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
+  const clientId =
+    "1064260607969-f2mrhl9a60tm9g81k2qc25jtjdc0uh01.apps.googleusercontent.com";
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-  const { loginUser } = useContext(AuthContext);
+  const { loginUser, registerUser } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     let err = await loginUser(email, password);
     setError(err);
+  };
+
+  const onGoogleSuccess = async (res) => {
+    const profile = res.profileObj;
+    let err = await loginUser(profile.email, profile.googleId);
+    if (err) {
+      console.log("Registering google user...");
+      await registerUser(
+        profile.givenName,
+        profile.familyName,
+        profile.email,
+        profile.googleId
+      );
+      console.log("Logging in google user now...");
+      await loginUser(profile.email, profile.googleId);
+    }
+  };
+
+  const onGoogleFailure = async (err) => {
+    console.log("Google signin failed", err);
   };
 
   return (
@@ -62,6 +88,14 @@ const LoginPage = () => {
           <br />
           <br />
         </form>
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign in with Google"
+          onSuccess={onGoogleSuccess}
+          onFailure={onGoogleFailure}
+          onRequest={console.log("Trying to log in google user...")}
+          cookiePolicy={"single_host_origin"}
+        />
 
         <p>
           Don't have an account? Register&nbsp;
