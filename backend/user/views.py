75cb7bc0886647c206850b8f7507f2c15e358784
lundsak <lundmarkisak@gmail.com>
2022-12-19 10:23:48 +0100

    Maybe fix change password bug

--- backend/user/views.py
+++ backend/user/views.py
@@ -20,6 +20,24 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsUserPermission,)
     http_method_names = ['get', 'put', 'delete', 'patch']
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        instance.set_password(serializer.data.get("password"))
+        instance.save()
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+
 
 class ChangePasswordView(generics.UpdateAPIView):
     serializer_class = ChangePasswordSerializer
